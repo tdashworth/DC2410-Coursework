b1e4aabd20f195253276fd64dc990108c21c586d
Tom Ashworth <thomas.d.ashworth@gmail.com>
2019-07-21 19:19:35 +0100

    Show whether an animal has been adopted

--- frontend/src/components/Animal/AnimalSummaryCard.tsx
+++ frontend/src/components/Animal/AnimalSummaryCard.tsx
@@ -5,20 +5,35 @@ import { IAnimal } from 'dc2410-coursework-common';
 class AnimalSummaryCard extends React.Component<{ animal: IAnimal }> {
   public render = () => (
     <div className="card mb-3">
-      <img src={this.props.animal.pictures![0]} className="card-img-top" alt="..." />
+      <img
+        src={this.props.animal.pictures![0]}
+        className="card-img-top"
+        alt="..."
+      />
       <div className="card-body">
         <h5 className="card-title">
-          <a href={`/animal/${this.props.animal.id}`}>{this.props.animal.name}</a>
+          <a href={`/animal/${this.props.animal.id}`}>
+            {this.props.animal.name}
+          </a>
         </h5>
         <p className="card-text">{this.props.animal.description}</p>
         <p className="card-text">
-          <a className="btn btn-primary text-light text-decoration-none" href={`/animal/${this.props.animal.id}`}>
+          <a
+            className="btn btn-primary text-light text-decoration-none"
+            href={`/animal/${this.props.animal.id}`}
+          >
             View more <Emoji symbol="👀" />
           </a>
         </p>
+
+        {this.props.animal.adoptedBy ? (
+          <div className="alert alert-success mb-0" role="alert">
+            Adopted!
+          </div>
+        ) : null}
       </div>
     </div>
-  )
+  );
 }
 
 export default AnimalSummaryCard;
